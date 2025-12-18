1cee78555f7c5a5010169b2ba51293b6097b04a7
Jeffrey Riaboy <jeff@riaboy.com>
2025-12-18 06:25:08 -0600

    COMPLETE overhaul of items.json, splitting up the Description field for clarity and the new Chain System:     Definition File: items.ebnf     Misc.json: Lists <StaticName> groups [Affects: PharloomAtlas.sh, Compile/shared]     items.json: Contains overhaul data

--- Compile/PharloomAtlas.sh
+++ Compile/PharloomAtlas.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source ./shared
-MakeZip PharloomAtlas.zip $(AddProject PharloomAtlas) $(AddProject SilkDev) "$plugin_dir/categories.json" "$plugin_dir/ItemFinder.json" "$plugin_dir/items.json"
+MakeZip PharloomAtlas.zip $(AddProject PharloomAtlas) $(AddProject SilkDev) "$plugin_dir/categories.json" "$plugin_dir/ItemFinder.json" "$plugin_dir/items.json" "$plugin_dir/Misc.json"
