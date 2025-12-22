fe1b3ac33edf054dcbee98d12e8fb4b631ce0dca
Jeffrey Riaboy <jeff@riaboy.com>
2025-12-22 16:24:52 -0600

    Removing files from master branch; Other minor edits
    Files getting moved to new locations:
        Misc.json, categories.json, items.json: silksong_mods_assets repository
        Convert.php: RemovedClasses branch
    
    .gitignore: Added a log file to the list
    Submit.php: Fixed alignment spacing

--- WebServer/Submit.php
+++ WebServer/Submit.php
@@ -3,10 +3,10 @@ require_once(__DIR__.'/Shared.php');
 
 const MAX_FIELD_LEN=100;
 $GetEscapedVariable=fn($Name, $Pattern) =>
-		!is_string($Val=($_REQUEST[$Name] ?? null)) ? ErrAndDie("Missing variable: $Name")
-		: ( !mb_check_encoding($Val, 'UTF-8') ? ErrAndDie("Improperly encoded variable: $Name")
-		: ( mb_strlen($Val)>MAX_FIELD_LEN ? ErrAndDie("Variable is too long: $Name")
-		: ( !preg_match($Pattern, $Val) ? ErrAndDie("Variable is not in the proper format or is too long: $Name")
+		!is_string($Val=($_REQUEST[$Name] ?? null))	? ErrAndDie("Missing variable: $Name")
+		: ( !mb_check_encoding($Val, 'UTF-8')		? ErrAndDie("Improperly encoded variable: $Name")
+		: ( mb_strlen($Val)>MAX_FIELD_LEN			? ErrAndDie("Variable is too long: $Name")
+		: ( !preg_match($Pattern, $Val)				? ErrAndDie("Variable is not in the proper format or is too long: $Name")
 		:   $Escape($Val)
 		)));
 
@@ -14,9 +14,9 @@ $GetEscapedVariable=fn($Name, $Pattern) =>
 try {
 	$Conn->query(sprintf(
 		"INSERT INTO SilkSongItems (ItemID, ItemName, Username) VALUES (%s, %s, %s)",
-		$GetEscapedVariable('ItemID', '/^[1-9]\d{0,6}$/D'),
-		$GetEscapedVariable('ItemName', '/^(?:PlayerData\.\w{1,45}|(?!PlayerData)[\w ]{1,40}\.[\w ()-]{1,45})$/D'),
-		($UserName=$GetEscapedVariable('Username', '/^[^\x00-\x1F\s][^\x00-\x1F\r\n]{2,49}$/uD'))
+		$GetEscapedVariable				('ItemID'  , '/^[1-9]\d{0,6}$/D'),
+		$GetEscapedVariable				('ItemName', '/^(?:PlayerData\.\w{1,45}|(?!PlayerData)[\w ]{1,40}\.[\w ()-]{1,45})$/D'),
+		($UserName=$GetEscapedVariable	('Username', '/^[^\x00-\x1F\s][^\x00-\x1F\r\n]{2,49}$/uD'))
 			!="'*SILKDEV NO NAME*'" ? $UserName
 			: $Escape('IP='.substr($_SERVER['HTTP_CF_CONNECTING_IP'] ?? $_SERVER['HTTP_X_REAL_IP'] ?? $_SERVER['REMOTE_ADDR'] ?? 'No IP address', 0, 47))
 	));
